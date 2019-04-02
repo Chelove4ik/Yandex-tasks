7a174c5ab3775c727877c23ab589e052e91818a0
Денис <dennis220802@gmail.com>
2019-04-02 13:42:59 +1000

    1

--- Урок WebServer 3/Личные дневники.py
+++ Урок WebServer 3/Личные дневники.py
@@ -1,17 +1,45 @@
-from flask import Flask, render_template
+from flask import render_template, Flask, session
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from werkzeug.utils import redirect
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import TextAreaField
 from wtforms.validators import DataRequired
-from Таблица_новостей import UsersModel, DB, NewsModel
+from Таблица_новостей import DB, NewsModel, UsersModel
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+db = DB()
 
 
 class AddNewsForm(FlaskForm):
     title = StringField('Заголовок новости', validators=[DataRequired()])
     content = TextAreaField('Текст новости', validators=[DataRequired()])
     submit = SubmitField('Добавить')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+
+        user_name = form.username.data
+        password = form.password.data
+        user_model = UsersModel(db.get_connection())
+        exists = user_model.exists(user_name, password)
+        if (exists[0]):
+            session['username'] = user_name
+            session['user_id'] = exists[1]
+        return redirect("/index")
+
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/dnevnik', methods=['GET', 'POST'])
@@ -22,11 +50,45 @@ def dnevnik():
     return render_template('add_news.html', title='Остаток от деления', form=form)
 
 
+@app.route('/logout')
+def logout():
+    session.pop('username', 0)
+    session.pop('user_id', 0)
+    return redirect('/login')
+
+
 @app.route('/')
-def ind():
-    db = DB()
-    users = NewsModel(db.get_connection())
-    return render_template('main.html', news=users.get_all())
+@app.route('/index')
+def index():
+    if 'username' not in session:
+        return redirect('/login')
+    news = NewsModel(db.get_connection()).get_all(session['user_id'])
+    return render_template('main.html', username=session['username'],
+                           news=news)
+
+
+@app.route('/add_news', methods=['GET', 'POST'])
+def add_news():
+    if 'username' not in session:
+        return redirect('/login')
+    form = AddNewsForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        nm = NewsModel(db.get_connection())
+        nm.insert(title, content, session['user_id'])
+        return redirect("/index")
+    return render_template('add_news.html', title='Добавление новости',
+                           form=form, username=session['username'])
+
+
+@app.route('/delete_news/<int:news_id>', methods=['GET'])
+def delete_news(news_id):
+    if 'username' not in session:
+        return redirect('/login')
+    nm = NewsModel(db.get_connection())
+    nm.delete(news_id)
+    return redirect("/index")
 
 
 if __name__ == '__main__':
