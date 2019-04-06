2e42fa977bc2baea51f987f174b9f8c4c3c6a657
Денис <dennis220802@gmail.com>
2019-04-06 18:14:03 +1000

    Сделана задача REST – знакомство

--- Урок WebServer 4/REST – знакомство/REST – знакомство.py
+++ Урок WebServer 4/REST – знакомство/REST – знакомство.py
@@ -6,18 +6,53 @@ from werkzeug.utils import redirect
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 from news_table import DB, UsersModel, NewsModel
-
+from flask import make_response
+from flask import request
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 db = DB()
 
 
-@app.route('/news',  methods=['GET'])
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.route('/news', methods=['GET'])
 def get_news():
     news = NewsModel(db.get_connection()).get_all()
     return jsonify({'news': news})
 
 
+@app.route('/news/<int:news_id>', methods=['GET'])
+def get_one_news(news_id):
+    news = NewsModel(db.get_connection()).get(news_id)
+    if not news:
+        return jsonify({'error': 'Not found'})
+    return jsonify({'news': news})
+
+
+@app.route('/news', methods=['POST'])
+def create_news():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in ['title', 'content', 'user_id']):
+        return jsonify({'error': 'Bad request'})
+    news = NewsModel(db.get_connection())
+    news.insert(request.json['title'], request.json['content'],
+                request.json['user_id'])
+    return jsonify({'success': 'OK'})
+
+
+@app.route('/news/<int:news_id>', methods=['DELETE'])
+def delete_news(news_id):
+    news = NewsModel(db.get_connection())
+    if not news.get(news_id):
+        return jsonify({'error': 'Not found'})
+    news.delete(news_id)
+    return jsonify({'success': 'OK'})
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080)
