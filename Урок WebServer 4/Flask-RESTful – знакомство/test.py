1e1a343df0ccc6c2f9463f8c61149edd8c3dd2d3
Денис <dennis220802@gmail.com>
2019-04-07 14:58:54 +1000

    Сделана задача "Flask-RESTful – знакомство"

---
+++ Урок WebServer 4/Flask-RESTful – знакомство/test.py
@@ -0,0 +1,17 @@
+from requests import get, post, delete
+
+print(get('http://localhost:8080/news').json())
+print(get('http://localhost:8080/news/5').json())
+print(get('http://localhost:8080/news/8').json())  # ошибка
+print(get('http://localhost:8080/news/q').json())  # ошибка
+print()
+print(post('http://localhost:8080/news').json())  # ошибка
+print(post('http://localhost:8080/news',
+           json={'title': 'Заголовок'}).json())  # ошибка
+print(post('http://localhost:8080/news',
+           json={'title': 'Заголовок',
+                 'content': 'Текст новости',
+                 'user_id': 1}).json())
+print()
+print(delete('http://localhost:8080/news/8').json())  # ошибка
+print(delete('http://localhost:8080/news/5').json())
