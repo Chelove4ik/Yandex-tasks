585dd3cf0cba55d8f8e6ed422fcf41b79d4d9f78
Денис <dennis220802@gmail.com>
2019-04-21 20:44:36 +1000

    Сделана задача "Полный поиск"

---
+++ Урок Проект API 2/Полный поиск.py
@@ -0,0 +1,60 @@
+import os
+import sys
+
+import pygame
+import requests
+from get_spn import get_spn
+
+# Пусть наше приложение предполагает запуск:
+# python search.py Москва, ул. Ак. Королева, 12
+# Тогда запрос к геокодеру формируется следующим образом:
+toponym_to_find = " ".join(sys.argv[1:])
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {"geocode": toponym_to_find, "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+if not response:
+    # обработка ошибочной ситуации
+    pass
+
+# Преобразуем ответ в json-объект
+json_response = response.json()
+# Получаем первый топоним из ответа геокодера.
+toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+# Координаты центра топонима:
+toponym_coodrinates = toponym["Point"]["pos"]
+# Долгота и широта:
+toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+# Собираем параметры для запроса к StaticMapsAPI:
+map_params = {
+    "ll": ",".join([toponym_longitude, toponym_lattitude]),
+    "spn": get_spn(toponym),
+    'pt': toponym_coodrinates.replace(' ', ',') + ',pm2rdl',
+    "l": "sat"
+}
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+# ... и выполняем запрос
+response = requests.get(map_api_server, params=map_params)
+
+map_file = "maps.jpg"
+try:
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+except IOError as ex:
+    print("Ошибка записи временного файла:", ex)
+    exit(2)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+clock = pygame.time.Clock()
+while pygame.event.wait().type != pygame.QUIT:
+    clock.tick(30)
+pygame.quit()
+
+os.remove(map_file)
