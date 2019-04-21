ab84304e34030773408f87b411d76f50518d1919
Денис <dennis220802@gmail.com>
2019-04-22 08:39:09 +1000

    Сделана задача "Поиск аптеки 2.0"

--- Урок Проект API 2/Поиск аптеки 2.0.py
+++ Урок Проект API 2/Поиск аптеки 2.0.py
@@ -1,12 +1,15 @@
 import os
+from sys import argv
 
 import pygame
 import requests
+from distance import lonlat_distance
 
 search_api_server = "https://search-maps.yandex.ru/v1/"
 api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
-address_ll = '37.6222,55.7566'
+# address_ll = '37.6222,55.7566'
+address_ll = ','.join(argv[1::])
 
 search_params = {
     "apikey": api_key,
@@ -34,22 +37,16 @@ org_address = organization["properties"]["CompanyMetaData"]["address"]
 # Получаем координаты ответа.
 point = organization["geometry"]["coordinates"]
 org_point = "{0},{1}".format(point[0], point[1])
-delta = "0.009"
 
 # Собираем параметры для запроса к StaticMapsAPI:
 map_params = {
-    # позиционируем карту центром на наш исходный адрес
-    "ll": address_ll,
-    "spn": ",".join([delta, delta]),
     "l": "map",
-    # добавим точку, чтобы указать найденную аптеку
-    "pt": "{0},pm2dgl".format(org_point)
+    "pt": "{0},pm2dgl~{1},pm2grm".format(org_point, address_ll)
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 # ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params)
-
 
 map_file = "maps.jpg"
 try:
@@ -60,8 +57,21 @@ except IOError as ex:
     exit(2)
 
 pygame.init()
-screen = pygame.display.set_mode((600, 450))
+screen = pygame.display.set_mode((1000, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
+
+myfont = pygame.font.SysFont('arial', 15)
+
+addr = myfont.render(''.join(org_address), 1, (255, 255, 255))
+name = myfont.render(organization['properties']['CompanyMetaData']['name'], 1, (255, 255, 255))
+time = myfont.render(organization['properties']['CompanyMetaData']['Hours']['text'], 1, (255, 255, 255))
+distance = myfont.render(str(int(lonlat_distance((address_ll.split(',')), point))) + ' метров', 1, (255, 255, 255))
+
+screen.blit(addr, (610, 0))
+screen.blit(name, (610, 20))
+screen.blit(time, (610, 40))
+screen.blit(distance, (610, 60))
+
 pygame.display.flip()
 clock = pygame.time.Clock()
 while pygame.event.wait().type != pygame.QUIT:
